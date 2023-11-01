9c2a4aad4eef723257c18861678959835d017571
Janmejay <janmejayspurohit@gmail.com>
2023-11-01 03:31:18 -0400

    complete geocoding

--- screens/Rides.js
+++ screens/Rides.js
@@ -1,28 +1,67 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const Rides = () => {
+  const [markerPosition, setMarkerPosition] = useState({
+    latitude: 39.1682449,
+    longitude: -86.5230073,
+  });
+
+  const handleMarkerDrag = (e) => {
+    setMarkerPosition(e.nativeEvent.coordinate);
+  };
+
+  const handleMapPress = (event) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setMarkerPosition({ latitude, longitude });
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <Text>Rides</Text>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        minLength={3}
+        autoFocus={true}
+        returnKeyType={"search"}
+        query={{ key: process.env.EXPO_PUBLIC_MAPS_API_KEY }}
+        fetchDetails={true}
+        onPress={(data, details = null) =>
+          setMarkerPosition({ latitude: details.geometry.location.lat, longitude: details.geometry.location.lng })
+        }
+        onFail={(error) => console.log(error)}
+        onNotFound={() => console.log("no results")}
+        debounce={300}
+        listEmptyComponent={() => (
+          <View style={{ flex: 1 }}>
+            <Text>No results were found</Text>
+          </View>
+        )}
+        styles={{
+          container: {
+            flex: 0,
+          },
+        }}
+      />
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: -34.603738,
-          longitude: -58.38157,
+          latitude: 39.1682449,
+          longitude: -86.5230073,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
+        onPress={handleMapPress}
+        region={{ ...markerPosition, latitudeDelta: 0.01, longitudeDelta: 0.01 }}
       >
         <Marker
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-          }}
-          title="Marker Title"
-          description="Marker Description"
+          coordinate={markerPosition}
+          title="Pin"
+          description="Drag and drop to pick a place"
+          draggable={true}
+          onDragEnd={handleMarkerDrag}
         />
       </MapView>
     </View>
